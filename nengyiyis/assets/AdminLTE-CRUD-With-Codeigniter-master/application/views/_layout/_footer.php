61c370b501a6b9ab106ae8245f741507454c93aa
vinafebrisani <94087488+vinafebrisani@users.noreply.github.com>
2022-02-03 11:47:45 +0700

    Delete nengyiyis directory

--- nengyiyis/assets/AdminLTE-CRUD-With-Codeigniter-master/application/views/_layout/_footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-	<!-- To the right -->
-	<div class="pull-right hidden-xs">
-		Dashboard Admin
-	</div>
-	<!-- Default to the left -->
-	<strong>Copyright &copy; 2016 <a href="#">Achmad Chadil Auwfar</a>.</strong> All rights reserved.
-</footer>
